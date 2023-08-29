8da5b72953749ebfc041770ee503500f7087520e
abubkr <rabubakar991@gmail.com>
2023-08-29 22:57:31 +0500

    build code changes

--- build.sh
+++ build.sh
@@ -2,9 +2,10 @@
 
 # Run makemigrations and migrate
 echo "Build Start"
-python3.10 -m pip install -r requirements.txt
-python3.10 manage.py makemigrations --noinput
-python3.10 manage.py migrate --noinput
+eche "Make migrations"
+python manage.py makemigrations --noinput
+echo "Migrating"
+python manage.py migrate --noinput
 echo "Build End"
 
 # Build the frontend (replace this with your actual frontend build command)
